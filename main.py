efac61db0318b3ebe394b38b7c3ec5af4992ede2
Noureddine AMRANE <amranenoureddin3@gmail.com>
2019-05-23 14:47:17 +0200

    clean code

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 file_content = open("file.txt", "r")
 out_put_file = open("out.txt", "w")
-index = 00000
+
+index = 0
+nbZero = 5
+
 for line in file_content:
   splitLine = line.split("=")
-  out = "C" + str(index).zfill(5) + "=" + splitLine[1]
+  out = "C" + str(index).zfill(nbZero) + "=" + splitLine[1]
   index += 1
   out_put_file.write(out)
+
 file_content.close()
 out_put_file.close()
